f55a13b25e7ad025deb8a167acc9e9ba93f47e8d
grapeapple-tree <84321396+grapeapple-tree@users.noreply.github.com>
2022-05-29 18:05:35 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+            function loadImage(obj) {
+              document.getElementById('preview').innerHTML = '<p>プレビュー</p>';
+              for (i = 0; i < obj.files.length; i++) {
+                var fileReader = new FileReader();
+                let isimage = /^image.*/.test(obj.files[i].type);
+                let filename = obj.files[i].name;
+                let filetype = obj.files[i].type;
+                if (filetype == "") {
+                  filetype = filename.split('.').pop() + "file";
+                }
+                let filesize = formatSize(obj.files[i].size);
+                fileReader.onload = (function(e) {
+                  if (isimage == true) {
+                    isimage = false;
+                    document.getElementById('preview').innerHTML +=
+                      '<div class="preitem">' +
+                      '<img src="' + e.target.result + '" style="width: 120px;"><br>' +
+                      '<label class="fname">' + filename + '</label>' +
+                      '<label>' + filetype + '</label><br>' +
+                      '<label>' + filesize + '</label>' +
+                      '</div>';
+                  } else {
+                    document.getElementById('preview').innerHTML +=
+                      '<div class="preitem">' +
+                      '<img src="https://04.si/favicon.png" style="width: 120px;"><br>' +
+                      '<label class="fname">' + filename + '</label>' +
+                      '<label>' + filetype + '</label><br>' +
+                      '<label>' + filesize + '</label>' +
+                      '</div>';
+                  }
+                });
+                fileReader.readAsDataURL(obj.files[i]);
+              }
+            }
